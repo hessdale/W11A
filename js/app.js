149f289d79b5726182f2579db16d28a668c4b153
dale <hessdale96@gmail.com>
2023-03-29 00:57:41 -0600

    added comment to my code and starting on bonus

--- js/app.js
+++ js/app.js
@@ -1,44 +1,40 @@
+//using variables to define each button on the index.html
 let apple = document.getElementById(`apple`);
 let banana = document.getElementById(`banana`);
 let peach = document.getElementById(`peach`);
-
-
-
+//using a object to define each with name/description/image
 let apple_info =
 {
     name: `apple`,
     description: `red`,
     image_url: `/images/apple.png`
-}
+};
 let banana_info = {
     name: `banana`,
     descirption: `yellow`,
     image_url: `/images/banana.png`
-}
+};
 let peach_info = {
     name: `peach`,
     descirption: `orange`,
     image_url: `/images/peach.png`
-}
+};
+//using variable of JSON to convert cookie info in functions
 let apple_JSON = JSON.stringify(apple_info);
 let banana_JSON = JSON.stringify(banana_info);
 let peach_JSON = JSON.stringify(peach_info);
 
-
+//functions that set the cookie info when a user makes selection
 function apple_clicked(details) {
     Cookies.set(`selection`, apple_JSON);
-}
-
+};
 function banana_clicked(details) {
     Cookies.set(`selection`, banana_JSON);
-}
+};
 function peach_clicked(details) {
     Cookies.set(`selection`, peach_JSON);
-}
-
-let cookie_selection = Cookies.get(`selection`);
-
+};
+//using addEventListener to execute code on click of specific button
 apple.addEventListener(`click`, apple_clicked);
 banana.addEventListener(`click`, banana_clicked);
 peach.addEventListener(`click`, peach_clicked);
-
--- js/selection.js
+++ js/selection.js
@@ -1,12 +1,12 @@
+//defining a variable that gets the info of cookie selection
 let cookie_selection = Cookies.get(`selection`);
-
-
-
+//using that variable in an if statement to see if its undefined and if it is then puts a message on the page
 if (cookie_selection === undefined) {
     document.body.insertAdjacentHTML(`afterend`, `<h1>Make a selection on home page</h1>`);
 };
-
+//using the variable in an if statement to add the selected fruits info on the page
 if (cookie_selection !== undefined) {
+    //defining the fruit info by converting the JSON using parse
     let fruit_info = JSON.parse(cookie_selection);
     document.body.insertAdjacentHTML(`afterend`, `<p>${fruit_info[`name`]}</p>`);
     document.body.insertAdjacentHTML(`afterend`, `<p>${fruit_info[`description`]}</p>`)
